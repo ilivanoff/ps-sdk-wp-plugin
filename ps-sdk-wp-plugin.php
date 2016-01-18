c0d4ea07f3cf6d739b427d510dae5f823a78077b
ilivanoff <ivanov.ilya.alex@gmail.com>
2016-01-19 00:15:37 +0300

    PsSdkWpPlugin

--- ps-sdk-wp-plugin.php
+++ ps-sdk-wp-plugin.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: Ps SDK Plugin
   ; Plugin URI: http://ivanovilya.com/
-  Description: Планиг включает в работу классы ps-sdk.
+  Description: Плагин включает в работу классы ps-sdk.
   Version: 1.0
   Author: ilivanoff
   Author URI: http://ivanovilya.com/
@@ -31,12 +31,11 @@
 // Make sure we don't expose any info if called directly
 // Same code as PsUtil::isWordPress()
 if (!function_exists('add_action') || !defined('ABSPATH')) {
-    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-    exit;
+    die('Hi there!  I\'m just a plugin, not much I can do when called directly.');
 }
 
 define('PSSDK_PLUGIN_VERSION', '1.0');
-define('PSSDK_PLUGIN_MINIMUM_WP_VERSION', '5.2');
+define('PSSDK_PLUGIN_MINIMUM_WP_VERSION', '3.2');
 define('PSSDK_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PSSDK_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('PSSDK_PLUGIN_LOG', PSSDK_PLUGIN_DIR . 'log.txt');
@@ -46,6 +45,22 @@ if (version_compare($GLOBALS['wp_version'], PSSDK_PLUGIN_MINIMUM_WP_VERSION, '<'
     die($message);
 }
 
+if (defined('PSSDK_PLUGIN_ENABLED')) {
+    //Плагин уже подключен
+} else {
+    define('PSSDK_PLUGIN_ENABLED', 1);
+
+    require_once ABSPATH . 'ps-includes/MainImportWp.php';
+
+    $BRIDGE = PsWpBridge::inst();
+
+    register_activation_hook(__FILE__, array($BRIDGE, 'pluginActivation'));
+    register_deactivation_hook(__FILE__, array($BRIDGE, 'pluginDeactivation'));
+
+    add_action('plugins_loaded', array($BRIDGE, 'init'));
+}
+
+/*
 function logg($msg) {
     @file_put_contents(PSSDK_PLUGIN_LOG, ($msg ? $msg : '') . "\n", FILE_APPEND);
 }
@@ -55,6 +70,7 @@ logg('REQUEST:' . print_r($_REQUEST, true));
 logg('__FILE__: ' . __FILE__);
 logg('PSSDK_PLUGIN_DIR: ' . PSSDK_PLUGIN_DIR);
 logg('PSSDK_PLUGIN_URL: ' . PSSDK_PLUGIN_URL);
+logg('DEFINED: ' . defined('XXXX'));
 
 register_activation_hook(__FILE__, array('PsSdkWpPlugin', 'plugin_activation'));
 register_deactivation_hook(__FILE__, array('PsSdkWpPlugin', 'plugin_deactivation'));
@@ -63,6 +79,8 @@ require_once( PSSDK_PLUGIN_DIR . 'class.ps-sdk-plugin.php' );
 //require_once( AKISMET__PLUGIN_DIR . 'class.akismet-widget.php' );
 
 add_action('init', array('PsSdkWpPlugin', 'init'));
+*/
+
 /*
 if ( is_admin() ) {
 	require_once( AKISMET__PLUGIN_DIR . 'class.akismet-admin.php' );
